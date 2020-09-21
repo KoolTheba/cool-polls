ee729742f38d093f341c11c38085f4f49a095d1a
Theba Gomez <gomez.teba@gmail.com>
2020-09-21 12:16:20 +0200

    feat(store): Added basic actions and reducers for authed users and users

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,62 +4,62 @@ import {
     _savePoll,
     _savePollAnswer
   } from './_DATA.js'
-  import { isObject } from './helpers'
+import { isObject } from './helpers'
   
-  function flattenPoll (poll) {
-    return Object.keys(poll)
-      .reduce((flattenedPoll, key) => {
-        const val = poll[key]
-  
-        if (isObject(val)) {
-          flattenedPoll[key + 'Text'] = val.text
-          flattenedPoll[key + 'Votes'] = val.votes
-          return flattenedPoll
-        }
-  
-        flattenedPoll[key] = val
+function flattenPoll (poll) {
+  return Object.keys(poll)
+    .reduce((flattenedPoll, key) => {
+      const val = poll[key]
+
+      if (isObject(val)) {
+        flattenedPoll[key + 'Text'] = val.text
+        flattenedPoll[key + 'Votes'] = val.votes
         return flattenedPoll
-      }, {})
-  }
-  
-  function formatPolls (polls) {
-    const pollIds = Object.keys(polls)
-  
-    return pollIds.reduce((formattedPolls, id) => {
-      formattedPolls[id] = flattenPoll(polls[id])
-      return formattedPolls
+      }
+
+      flattenedPoll[key] = val
+      return flattenedPoll
     }, {})
-  }
+}
+
+function formatPolls (polls) {
+  const pollIds = Object.keys(polls)
+
+  return pollIds.reduce((formattedPolls, id) => {
+    formattedPolls[id] = flattenPoll(polls[id])
+    return formattedPolls
+  }, {})
+}
   
-  function formatUsers (users) {
-    return Object.keys(users)
-      .reduce((formattedUsers, id) => {
-        const user = users[id]
+function formatUsers (users) {
+  return Object.keys(users)
+    .reduce((formattedUsers, id) => {
+      const user = users[id]
+
+      formattedUsers[id] = {
+        ...user,
+        answers: Object.keys(user.answers)
+      }
+
+      return formattedUsers
+    }, {})
+}
+
+export function getInitialData () {
+  return Promise.all([
+    _getUsers(),
+    _getPolls(),
+  ]).then(([users, polls]) => ({
+    users: formatUsers(users),
+    polls: formatPolls(polls),
+  }))
+}
+
+export function savePoll (poll) {
+  return _savePoll(poll)
+    .then((p) => flattenPoll(p))
+}
   
-        formattedUsers[id] = {
-          ...user,
-          answers: Object.keys(user.answers)
-        }
-  
-        return formattedUsers
-      }, {})
-  }
-  
-  export function getInitialData () {
-    return Promise.all([
-      _getUsers(),
-      _getPolls(),
-    ]).then(([users, polls]) => ({
-      users: formatUsers(users),
-      polls: formatPolls(polls),
-    }))
-  }
-  
-  export function savePoll (poll) {
-    return _savePoll(poll)
-      .then((p) => flattenPoll(p))
-  }
-  
-  export function savePollAnswer (args) {
-    return _savePollAnswer(args)
-  }
+export function savePollAnswer (args) {
+  return _savePollAnswer(args)
+}
